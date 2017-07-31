fce40d6111a4faa9c04318cffc0d8c780e6fd9e9
Artem Peregut <artem.peregut@gmail.com>
2017-07-31 10:58:21 +0300

    Yandex Pages

--- src/main/java/pages/MarketPage.java
+++ src/main/java/pages/MarketPage.java
@@ -1,4 +1,22 @@
 package pages;
 
-public class MarketPage {
+import net.serenitybdd.core.annotations.findby.FindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class MarketPage extends BasePageObject {
+
+    @FindBy(xpath = "//ul[@class='topmenu__list']")
+    WebElement topMenuItems;
+
+    @FindBy(xpath = "//div[@class='catalog-menu i-bem catalog-menu_js_inited']")
+    WebElement catalogMenuItems;
+
+    public void selectTopMenuItem(String itemName) {
+        topMenuItems.findElement(By.xpath("//li[@data-department='" + itemName + "']")).click();
+    }
+
+    public void selectCatalogMenuItem(String itemName) {
+        catalogMenuItems.findElement(By.xpath("//div[@class='catalog-menu__list']/a[contains(text(),'" + itemName + "')]")).click();
+    }
 }
--- src/main/java/pages/YandexPage.java
+++ src/main/java/pages/YandexPage.java
@@ -1,4 +1,15 @@
 package pages;
 
-public class YandexPage {
+import net.serenitybdd.core.annotations.findby.FindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class YandexPage extends BasePageObject {
+
+    @FindBy(xpath = "//div[@role='navigation']")
+    WebElement navigationTabs;
+
+    public void selectTab(String itemName) {
+        navigationTabs.findElement(By.xpath("//a[text()='" + itemName + "']")).click();
+    }
 }
