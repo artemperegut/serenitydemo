76cfc8fa8bb9a6612eba9283c62944fe22e15c6f
Artem Peregut <artem.peregut@gmail.com>
2017-07-31 12:19:35 +0300

    Yandex Market Test

--- src/test/java/YandexMarketTest.java
+++ src/test/java/YandexMarketTest.java
@@ -1,2 +1,37 @@
-public class YandexMarketTest {
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.BaseSteps;
+import steps.MarketSteps;
+import steps.TvListSteps;
+import steps.YandexSteps;
+
+@RunWith(SerenityRunner.class)
+public class YandexMarketTest extends BaseSteps {
+
+    @Steps
+    MarketSteps marketSteps;
+
+    @Steps
+    TvListSteps tvListSteps;
+
+    @Steps
+    YandexSteps yandexSteps;
+
+    @Title("Тестирование Яндекс.Маркет")
+    @Test
+    public void test() {
+
+        yandexSteps.selectTabStep("Маркет");
+        marketSteps.selectMenuItemStep("Электроника");
+        marketSteps.selectCatalogMenuItemStep("Телевизоры");
+        tvListSteps.fillPriceFromStep("20000");
+        tvListSteps.selectProducerStep("Samsung");
+        tvListSteps.selectProducerStep("LG");
+        tvListSteps.confirmStep();
+        tvListSteps.checkTvListStep(12);
+        
+    }
 }
